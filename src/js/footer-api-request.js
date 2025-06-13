83b3136647c58d49cd2709394af5597f963ce8af
Maxim_Boiko <Maksimboiko222@gmail.com>
2025-06-13 17:08:52 +0200

    update

--- src/js/footer-api-request.js
+++ src/js/footer-api-request.js
@@ -1,12 +1,10 @@
 import AXIOS from 'axios';
 
-// axios instance
 const axios = AXIOS.create({
   baseURL: 'https://portfolio-js.b.goit.study/api',
   timeout: 1000,
 });
 
-// endpoint
 const endPoint = '/requests';
 
 export function doPostRequest(email, comment) {
